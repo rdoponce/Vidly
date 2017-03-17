2fac738f177dfc0c715195931eb7675fec8eba42
ricardo <ricardo.ponce.galaz@gmail.com>
2017-03-17 04:02:35 -0300

    Mensaje

---
+++ Vidly/Models/IdentityModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class IdentityModels : DbContext
+    {
+        public DbSet<Customer> Customers {get; set;}
+
+        public IdentityModels()
+            : base("Vidly.Models.IdentityModels")
+        {
+        }
+
+        public static DbContext Create()
+        {
+            return new IdentityModels();
+        }
+    }
+}
